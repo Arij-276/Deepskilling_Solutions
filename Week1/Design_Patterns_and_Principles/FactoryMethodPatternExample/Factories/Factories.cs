a907f084a364d3a1e0b771f634b676f825fd0055
Arij <2205276@kiit.ac.in>
2025-06-22 17:55:00 +0530

    Week 1 Ex 2

---
+++ Week1/Design_Patterns_and_Principles/FactoryMethodPatternExample/Factories/Factories.cs
@@ -0,0 +1,23 @@
+﻿// DocumentFactory.cs
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+// WordFactory.cs
+public class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument() => new WordDocument();
+}
+
+// PdfFactory.cs
+public class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument() => new PdfDocument();
+}
+
+// ExcelFactory.cs
+public class ExcelFactory : DocumentFactory
+{
+    public override IDocument CreateDocument() => new ExcelDocument();
+}
---
+++ Week1/Design_Patterns_and_Principles/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,32 @@
+﻿// Program.cs
+class Program
+{
+    static void Main()
+    {
+        // Create factories
+        DocumentFactory wordFactory = new WordFactory();
+        DocumentFactory pdfFactory = new PdfFactory();
+        DocumentFactory excelFactory = new ExcelFactory();
+
+        // Create documents using factories
+        IDocument wordDoc = wordFactory.CreateDocument();
+        IDocument pdfDoc = pdfFactory.CreateDocument();
+        IDocument excelDoc = excelFactory.CreateDocument();
+
+        // Test document operations
+        Console.WriteLine("Testing Word Document:");
+        wordDoc.Open();
+        wordDoc.Save();
+        wordDoc.Close();
+
+        Console.WriteLine("\nTesting PDF Document:");
+        pdfDoc.Open();
+        pdfDoc.Save();
+        pdfDoc.Close();
+
+        Console.WriteLine("\nTesting Excel Document:");
+        excelDoc.Open();
+        excelDoc.Save();
+        excelDoc.Close();
+    }
+}
