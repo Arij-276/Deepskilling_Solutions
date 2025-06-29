01cc5e87de57a7208812835ce4521629a9c105c6
Arij <2205276@kiit.ac.in>
2025-06-29 21:18:50 +0530

    Final

---
+++ Week-2/NUnit_and_Moq/Moq/CustomerCommLib.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommLib.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void SendMailToCustomer_WhenCalled_ShouldReturnTrue()
+        {
+            // Arrange
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var customerComm = new CustomerComm(mockMailSender.Object);
+
+            // Act
+            bool result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.IsTrue(result);
+            mockMailSender.Verify(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
+
---
+++ Week1/Algorithms_Data_Structures/InventoryManagementSystem/Product.cs
@@ -0,0 +1,15 @@
+﻿public class Product
+{
+    public int ProductId { get; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(int id, string name, int quantity, decimal price)
+    {
+        ProductId = id;
+        ProductName = name;
+        Quantity = quantity;
+        Price = price;
+    }
+}
